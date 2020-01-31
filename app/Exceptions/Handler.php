cddda3f68f95af030a3ce47d654b8f2c5e22ad59
Anthony Le Courric <freelance.anthony@gmail.com>
2020-01-31 11:30:45 +0100

    Manage Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,78 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Sorry, this {$modelName} doesn't exist.", 404);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse("Sorry, this route doesn't exist.", 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse("Sorry, method not allowed.", 405);
+        }
+        
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException) {
+            if(isset($exception->errorInfo)){
+                $errorCode = $exception->errorInfo[1];
+
+                if ($errorCode == 1451) {
+                    return $this->errorResponse("Cannot remove this ressource permanently, it is related with another ressource.", 409);
+                }
+            }
+        }
+        
+        return (config("app.debug"))
+            ? parent::render($request, $exception)
+            : $this->errorResponse("Unexpected exception. Retry later", 500);
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        if ($e->response) {
+            return $e->response;
+        }
+
+        return $this->errorResponse($e->getMessage(), $e->status, $e->errors());
+    }
 }
